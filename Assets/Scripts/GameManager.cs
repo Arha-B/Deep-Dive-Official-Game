dcc4879e8d58372442146f00ca80769968aeeee3
Arha B <arhabangera@gmail.com>
2026-03-09 14:01:17 -0400

    Just working on the scripts

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,52 +1,33 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance { get; private set; }
+    public static GameManager Instance;
 
-    [Header("Game State")]
     public bool hasTreasure = false;
-    public bool isGameOver = false;
-    
-    // Depth tracking (Optional, for UI)
-    public float currentDepth; 
+    public bool isGameOver  = false;
 
-    private void Awake()
+    void Awake()
     {
-        if (Instance != null && Instance != this) Destroy(gameObject);
-        else Instance = this;
+        if (Instance != null) { Destroy(gameObject); return; }
+        Instance = this;
     }
 
     public void GrabTreasure()
     {
         hasTreasure = true;
-        Debug.Log("TREASURE ACQUIRED! The Fih are becoming aggressive!");
-        // Triggers global alarm or music change here
-    }
-
-    public void GameOver()
-    {
-        if (isGameOver) return;
-        isGameOver = true;
-        Debug.Log("Game Over: Blown up or Eaten.");
-        // Show Game Over UI
-        Time.timeScale = 0; 
+        Debug.Log("Treasure grabbed!");
     }
 
     public void WinGame()
     {
-        if (isGameOver) return;
-        isGameOver = true;
-        Debug.Log("Surface Reached! You Win!");
-        // Show Win UI
-        Time.timeScale = 0; 
+        Debug.Log("You win!");
     }
 
-    public void RestartLevel()
+    public void GameOver()
     {
-        Time.timeScale = 1;
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        isGameOver = true;
+        Debug.Log("Game over!");
     }
 }
-
-
