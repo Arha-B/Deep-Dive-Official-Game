cf6ca8948f67637d4cc382689890a56ed6660798
Neel <neeladityasolanki@gmail.com>
2026-03-09 00:39:35 -0400

    added fih controller

---
+++ Assets/Scripts/FihController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Fih : MonoBehaviour
+{
+    public int TypeOfFih = 1; // 1 = normal, 2 = big, 3 = dart
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
